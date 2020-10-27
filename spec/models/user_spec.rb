ebd62b3ab67850213af7e1c8c117b5d5a427ea6a
Tripp Martin <tripp.martin3@gmail.com>
2020-10-27 18:25:52 -0400

    Building out test for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,5 +11,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create :user }
+  it { is_expected.to validate_uniqueness_of :name }
 end
