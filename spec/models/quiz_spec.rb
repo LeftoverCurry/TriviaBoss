570ca794dbe668da21a5de4366bb980342eb50e9
Tripp Martin <tripp.martin3@gmail.com>
2020-10-28 14:24:23 -0400

    ran migrations for quiz and devise, added name to user

--- spec/models/quiz_spec.rb
+++ spec/models/quiz_spec.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: quizzes
+#
+#  id         :bigint           not null, primary key
+#  score      :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_quizzes_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 require 'rails_helper'
 
 RSpec.describe Quiz, type: :model do
