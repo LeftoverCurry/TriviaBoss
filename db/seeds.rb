84f6e8a262b630fe5fa5bc0d4d410484938b4ec9
Tripp Martin <tripp.martin3@gmail.com>
2020-10-29 15:44:53 -0400

    simplified seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+# Seedfile populates the Database with data provided for the code challenge.
+
+# Parses the provided JSON file
+trivia = JSON.parse(File.read('db/seed_data/trivia.json'))
+
+# Iterates over the resulting hash and creates a new Trivium object for each one.
+trivia.each do |trivium|
+  Trivium.find_or_create_by!(trivium)
+end
