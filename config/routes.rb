191f4e944fd72aeafbaed5bfe6d39e5807fa62aa
Tripp Martin <tripp.martin3@gmail.com>
2020-10-29 20:37:48 -0400

    created quizzes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :quizzes, only: %i[index new show create]
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'quiz#index'
+  root to: 'quizzes#index'
 end
