b59f4079893778bb13d5d74a0ff26f8427bac0b1
Tripp Martin <tripp.martin3@gmail.com>
2020-10-30 15:31:48 -0400

    corrected routes to new model

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 Rails.application.routes.draw do
   root to: 'quizzes#index'
   get 'answer', to: 'question#show', as: :answer
+  get 'question', to: 'question#edit', as: :ask_question
   put 'respond', to: 'question#update', as: :respond
   resources :quizzes, only: %i[index show create]
   resources :questions, only: %i[edit update show]
