b0adc3d3825d85fec369b1f0ad1317b3cfd9eb19
Tripp Martin <tripp.martin3@gmail.com>
2020-10-30 10:07:20 -0400

    updated route naming

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root to: 'quizzes#index'
+  get 'question', to: 'questions#edit', as: :question
+  get 'answer', to: 'questions#show', as: :answer
+  put 'respond', to: 'questions#update', as: :respond
   resources :quizzes, only: %i[index new show create]
   resources :questions, controller: 'quiz_trivia', only: %i[edit update show]
 
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'quizzes#index'
 end
