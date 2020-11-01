2e43afdc3e3634a65749b6dbf2e66de65048e062
Tripp Martin <tripp.martin3@gmail.com>
2020-10-31 21:45:38 -0400

    added desired text to views

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,7 +9,7 @@ class QuestionsController < ApplicationController
   def show
     quiz_questions = Question.where(quiz_id: @question.quiz_id)
     available_questions = quiz_questions.where(response: [nil, ''])
-
+    @previous_question = @question
     @done = available_questions.empty?
     @score = quiz_questions.where(is_correct: true).count
     @quiz = @question.quiz
@@ -24,7 +24,7 @@ class QuestionsController < ApplicationController
   # PATCH/PUT /respond
   def update
     @question.update_attributes(is_correct: @question.trivium.correct ==
-                                            (params[:response]),
+                                            (question_params[:response]),
                                 response: question_params['response'])
     redirect_to @question
   end
