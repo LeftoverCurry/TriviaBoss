8b35e153210c8fc955f849b13b5d2c9063f9ae23
Tripp Martin <tripp.martin3@gmail.com>
2020-11-01 15:35:34 -0500

    QA changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@
 #  id                     :bigint           not null, primary key
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
-#  name                   :string           not null
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
@@ -22,6 +21,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :name, presence: true, uniqueness: true
   has_many :quizzes
 end
