c50244413414c4228567dadc8831cc164ecb4509
Tripp Martin <tripp.martin3@gmail.com>
2020-11-01 15:59:52 -0500

    removed conflicting migrations

--- db/migrate/20201029235013_add_response_to_quiz_trivium.rb
+++
@@ -1,6 +0,0 @@
-class AddResponseToQuizTrivium < ActiveRecord::Migration[6.0]
-  def change
-    add_column :quiz_trivia, :response, :string
-    add_column :quiz_trivia, :is_correct?, :boolean
-  end
-end
